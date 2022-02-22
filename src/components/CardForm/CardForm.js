067246fea0fb3cc9c2bdc81c80cd27ff2354fe65
Maria Wierzbicka <maria.a.wierzbicka@gmail.com>
2022-02-22 13:12:03 +0100

    Add CardForm

---
+++ src/components/CardForm/CardForm.js
@@ -0,0 +1,27 @@
+import Button from '../Button/Button';
+import TextInput from './../TextInput/TextInput';
+import { useState } from 'react';
+import styles from './CardForm.module.scss';
+
+const CardForm = (props) => {
+
+  const [title, setTitle] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    props.action({ title: title }, props.columnId);
+    setTitle('');
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className={styles.cardForm}>
+      <TextInput value={title} onChange={e => setTitle(e.target.value)} />
+      <Button>
+        Add card
+      </Button>
+    </form>
+  )
+  
+}
+
+export default CardForm;
